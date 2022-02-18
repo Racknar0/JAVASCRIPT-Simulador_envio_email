308d264dc65a82b43e2a3040a2c2b5c1ca05c20f
Racknar0 <racknarow1@gmail.com>
2022-02-17 22:12:29 -0500

    Add Class Red Tailwind

--- js/app.js
+++ js/app.js
@@ -1,16 +1,35 @@
 //Variables
 const btnEnviar = document.querySelector('#enviar');
 
+//variables para campos
+const email = document.querySelector('#email');
+const asunto = document.querySelector('#asunto');
+const mensaje = document.querySelector('#mensaje');
 
 
 eventListeners();
 function eventListeners() {
-    document.addEventListener('DOMContentLoaded', iniciarApp);
+    document.addEventListener('DOMContentLoaded', iniciarApp); //!iniciando app
+
+    email.addEventListener('blur', validarFormulario);
+    asunto.addEventListener('blur', validarFormulario);
+    mensaje.addEventListener('blur', validarFormulario);
 }
+
 
 
 //funciones
 function iniciarApp() {
     btnEnviar.disabled = true;
     btnEnviar.classList.add('cursor-not-allowed', 'opacity-50');
+}
+
+//validarFormulario
+function validarFormulario(e) {
+    if (e.target.value.length > 0) {
+        console.log('si hay algo');
+    } else {
+        e.target.classList.add('border', 'border-red-500');
+    }
+    
 }
