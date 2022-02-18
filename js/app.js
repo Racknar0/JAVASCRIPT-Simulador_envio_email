9be2e6819ebfe045cd8715c2e037af297ade2c80
Racknar0 <racknarow1@gmail.com>
2022-02-17 23:09:24 -0500

    Add Validator Incomplete

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 //Variables
 const btnEnviar = document.querySelector('#enviar');
+const formulario = document.querySelector('#enviar-mail');
 
 //variables para campos
 const email = document.querySelector('#email');
@@ -26,10 +27,45 @@ function iniciarApp() {
 
 //validarFormulario
 function validarFormulario(e) {
-    if (e.target.value.length > 0) {
-        console.log('si hay algo');
-    } else {
-        e.target.classList.add('border', 'border-red-500');
-    }
     
+    if (e.target.value.length > 0) {
+
+        // Elimina el mensaje de error del dom
+        const error = document.querySelector('p.error')
+        error.remove();
+
+        e.target.classList.remove('border', 'border-red-500');
+        e.target.classList.add('border', 'border-green-500');
+
+    } else {
+        e.target.classList.remove('border', 'border-green-500');
+        e.target.classList.add('border', 'border-red-500');
+    
+        mostrarError('Todos los campos son obligatorios');
+    }
+
+    if (e.target.type === 'email') {
+        const er = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+        if(er.test( e.target.value )) {
+            e.target.classList.remove('border', 'border-red-500');
+            e.target.classList.add('border', 'border-green-500');
+            error.remove();
+        } else {
+            e.target.classList.remove('border', 'border-green-500');
+             e.target.classList.add('border', 'border-red-500');
+            mostrarError('Email no valido');
+        }
+    }
+}
+
+function mostrarError(mensaje) {
+    const mensajeError = document.createElement('P')
+    mensajeError.textContent = mensaje;
+    mensajeError.classList.add('border', 'border-red-500', 'backgroud-red-100', 'text-red-500', 'p-3', 'mt-5', 'text-center', 'error');
+
+    const errores = document.querySelectorAll('.error')
+    if (errores.length === 0) {
+        formulario.appendChild(mensajeError)
+    } 
 }
